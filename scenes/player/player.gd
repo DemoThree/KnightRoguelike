1a1b7bc2ec9af53ddc63b284a86a23ce79e28cbb
DemoThree <89907405+DemoThree@users.noreply.github.com>
2025-01-27 22:44:36 +0800

    add:camera,tilemap and so on

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,11 +1,29 @@
 extends CharacterBody2D
+# 创建一个速度常量
+const MAX_SPEED = 200
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+		pass
+		# Engine.max_fps = 20
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+		# 设置变量获取按键的二维向量
+		var movement_vector = get_movement_vector()
+		# 将二维向量归一化
+		var direction = movement_vector.normalized()
+		# 设置角色移动的向量速度
+		velocity = direction * MAX_SPEED
+		print(velocity)
+		# 让角色移动起来
+		move_and_slide()
+		
+func get_movement_vector():
+		
+		#创建变量x_movement用来获取是否在X轴向左还是向右移动
+		var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+		
+		#创建变量y_movement用来获取是否在X轴向上还是向下移动
+		var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+		
+		return Vector2(x_movement,y_movement) #返回x和y的值
